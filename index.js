227587d1df1615881faaa7f67cc47f20fff8ef31
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-06-28 14:47:15 -0400

    project complete, game is functional

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ var guessField = document.querySelector('.guessField');
 var guessCount = 1;
 var resetButton;
 
+guessField.focus();
+
 function checkGuess() {
     var userGuess = Number(guessField.value);
     if (guessCount === 1) {
         guesses.textContent = 'Previous guesses: ';
     }
-    guesses.textContent += userGuess + '';
+    guesses.textContent += userGuess + ' ';
     guesses.style.backgroundColor = 'blue';
     if (userGuess === randomNumber) {
         lastResult.textContent = 'Congratulations! You got it right!';
         lastResult.style.backgroundColor = 'green';
-        lowOrHi.textContent = '';
+        lowOrHi.textContent = ' ';
         setGameOver();
     } else if (guessCount === 10) {
         lastResult.textContent = '!!!GAME OVER!!!';
-        lowOrHi.textContent = '';
+        lowOrHi.textContent = ' ';
         setGameOver();
     } else {
         lastResult.textContent = 'Wrong!';
@@ -34,6 +36,37 @@ function checkGuess() {
     }
 
     guessCount++;
-    guessField.value = '';
+    guessField.value = ' ';
     guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+    var resetParas = document.querySelectorAll('.resultParas p');
+    for (var i = 0; i < resetParas.length; i++) {
+        resetParas[i].textContent = ' ';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = ' ';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = '#12aab4';
+    guesses.style.backgroundColor = '#12aab4';
+
+    randomNumber = Math.floor(Math.random() * 500) + 1;
 }
